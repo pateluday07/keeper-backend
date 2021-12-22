df7984e8e0b2cf912b950a3d1a929f624e25cd15
pateluday07 <pateluday07@gmail.com>
2021-12-22 20:44:10 +0530

    initial commit

---
+++ src/main/java/com/keeper/KeeperBackendApplication.java
@@ -0,0 +1,13 @@
+package com.keeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeeperBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeeperBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keeper/KeeperBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeeperBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
