0b45e265377dc95a9b5f0ba4f4d089d11ec72619
pateluday07 <pateluday07@gmail.com>
2022-02-26 12:09:49 +0530

    integration test for save note

---
+++ src/test/java/com/keeper/constants/TestConstants.java
@@ -0,0 +1,16 @@
+package com.keeper.constants;
+
+public class TestConstants {
+
+    private TestConstants(){
+
+    }
+
+    public static final String URL = "http://localhost:";
+
+    //EXCEPTION CONSTANTS
+    public static final String EXCEPTION_STATUS_KEY = "status";
+    public static final String EXCEPTION_PATH_KEY = "path";
+    public static final String EXCEPTION_MESSAGE_KEY = "message";
+
+}
--- src/test/java/com/keeper/controller/NoteControllerIntegrationTest.java
+++ src/test/java/com/keeper/controller/NoteControllerIntegrationTest.java
@@ -1,24 +1,26 @@
 package com.keeper.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.keeper.dto.NoteDTO;
 import com.keeper.service.NoteService;
+import com.keeper.util.MessageSourceUtil;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
+import static com.keeper.constant.MessagePropertyConstant.NOTE_ID_NOT_NULL;
+import static com.keeper.constants.TestConstants.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -27,19 +29,25 @@ import static org.junit.jupiter.api.Assertions.*;
 class NoteControllerIntegrationTest {
 
     private static final String API_PREFIX = "/api/notes/";
-    private static final String URL = "http://localhost:";
-    private static final TestRestTemplate testRestTemplate = new TestRestTemplate();
-
-    private static List<NoteDTO> noteDTOS;
+    private static final TestRestTemplate REST_TEMPLATE = new TestRestTemplate();
+    private static final HttpHeaders HTTP_HEADERS = new HttpHeaders();
 
     @Autowired
     private NoteService noteService;
+    @Autowired
+    private MessageSourceUtil messageSourceUtil;
 
     @LocalServerPort
     private int port;
+    private List<NoteDTO> noteDTOS;
 
     @BeforeAll
-    static void prepareNoteDTO() {
+    static void init() {
+        HTTP_HEADERS.setContentType(MediaType.APPLICATION_JSON);
+    }
+
+    @BeforeEach
+    void prepareNoteDTO() {
         NoteDTO noteOne = new NoteDTO();
         noteOne.setTitle("One Title");
         noteOne.setDescription("One Description");
@@ -52,13 +60,21 @@ class NoteControllerIntegrationTest {
     }
 
     @Test
-    void ifNoteIdNotNull_whenSaveNote_thenThrowBadRequestException(){
-
+    void ifNoteIdNotNull_whenSaveNote_thenThrowBadRequestException() {
+        NoteDTO noteDTO = noteDTOS.get(0);
+        noteDTO.setId(1L);
+        ResponseEntity<JsonNode> exception = REST_TEMPLATE.postForEntity(URL + port + API_PREFIX,
+                new HttpEntity<>(noteDTO, HTTP_HEADERS), JsonNode.class);
+        assertTrue(exception.hasBody());
+        assertEquals(HttpStatus.BAD_REQUEST, exception.getStatusCode());
+        assertEquals(HttpStatus.BAD_REQUEST.value(), Objects.requireNonNull(exception.getBody()).get(EXCEPTION_STATUS_KEY).asInt());
+        assertEquals(API_PREFIX, exception.getBody().get(EXCEPTION_PATH_KEY).asText());
+        assertEquals(messageSourceUtil.getMessage(NOTE_ID_NOT_NULL), exception.getBody().get(EXCEPTION_MESSAGE_KEY).asText());
     }
 
     @Test
     void whenNotesNotAvailable_thenListShouldBeEmpty() {
-        ResponseEntity<List<NoteDTO>> response = testRestTemplate.exchange(URL + port + API_PREFIX, HttpMethod.GET, null,
+        ResponseEntity<List<NoteDTO>> response = REST_TEMPLATE.exchange(URL + port + API_PREFIX, HttpMethod.GET, null,
                 new ParameterizedTypeReference<>() {
                 });
         assertEquals(HttpStatus.OK, response.getStatusCode());
@@ -70,7 +86,7 @@ class NoteControllerIntegrationTest {
     void whenNotesAvailable_thenListShouldNotBeEmpty() {
         noteService.save(noteDTOS.get(0));
         noteService.save(noteDTOS.get(1));
-        ResponseEntity<List<NoteDTO>> response = testRestTemplate.exchange(URL + port + API_PREFIX, HttpMethod.GET, null,
+        ResponseEntity<List<NoteDTO>> response = REST_TEMPLATE.exchange(URL + port + API_PREFIX, HttpMethod.GET, null,
                 new ParameterizedTypeReference<>() {
                 });
         assertEquals(HttpStatus.OK, response.getStatusCode());
